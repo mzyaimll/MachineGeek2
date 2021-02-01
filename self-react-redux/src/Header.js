4a5feb3590804169e5f3ec8657afd5da7a09fcfb
mzyaimll <2592972482@qq.com>
2021-02-01 18:19:30 +0800

    update make-redux

---
+++ self-react-redux/src/Header.js
@@ -0,0 +1,34 @@
+/*
+ * @Autor: GeekMzy
+ * @Date: 2021-02-01 17:29:57
+ * @LastEditors: GeekMzy
+ * @LastEditTime: 2021-02-01 17:35:56
+ * @FilePath: /MachineGeek2/self-react-redux/src/Header.js
+ */
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+class Header extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+  constructor() {
+    super()
+    this.state = { themeColor: '' }
+  }
+  componentWillMount () {
+    const { store } = this.context
+    this._updateThemeColor()
+    store.subscribe(() => this._updateThemeColor())
+  }
+  _updateThemeColor () {
+    const { store } = this.context
+    const state = store.getState()
+    this.setState({ themeColor: state.themeColor })
+  }
+  render () {
+    return (
+      <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
+    )
+  }
+}
+export default Header
---
+++ self-react-redux/src/ThemeSwitch.js
@@ -0,0 +1,49 @@
+/*
+ * @Autor: GeekMzy
+ * @Date: 2021-02-01 17:30:11
+ * @LastEditors: GeekMzy
+ * @LastEditTime: 2021-02-01 17:35:47
+ * @FilePath: /MachineGeek2/self-react-redux/src/ThemeSwitch.js
+ */
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+class ThemeSwitch extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+  constructor() {
+    super()
+    this.state = { themeColor: '' }
+  }
+  componentWillMount () {
+    const { store } = this.context
+    this._updateThemeColor()
+    store.subscribe(() => this._updateThemeColor())
+  }
+  _updateThemeColor () {
+    const { store } = this.context
+    const state = store.getState()
+    this.setState({ themeColor: state.themeColor })
+  }
+  // dispatch action 去改变颜色
+  handleSwitchColor (color) {
+    const { store } = this.context
+    store.dispatch({
+      type: 'CHANGE_COLOR',
+      themeColor: color
+    })
+  }
+  render () {
+    return (
+      <div>
+        <button
+          style={{ color: this.state.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
+        <button
+          style={{ color: this.state.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'blue')}>Blue</button>
+      </div>
+    )
+  }
+}
+export default ThemeSwitch
